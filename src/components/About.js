d7be562060ae9690c277104a143ce62c8f216600
Edita Stralkute <editastralkute@editas-air.home>
2020-03-31 19:55:07 +0100

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,16 @@ import React from "react";
 const About = () => (
 	<>
 		<h1>About</h1>
-		<p>
-		A little about myself:
+		
+		<p>A little about myself:</p>
 
-I have been in retail for 10 years working from a part time shelf stacker to a manager of over 30 people, navigating throught 3 respectable UK companies.
+<p>I have been in retail for 10 years working from a part time shelf stacker to a manager of over 30 people, navigating throught 3 respectable UK companies.
 Once i heard about web development in August 2019 i tried out a couple of courses and was immediately hooked. That month i quit my position in retail and started to spend serious time 
 learning more about it. During that same month i found out that my wife fell pregnant and i knew that i needed to get a job to provide for our growing family. 
 Now that we have had our little baby boy i have managed to put aside all other work again and concentrate on my learning. 
-This website has been created using Gatsby to document my development in web development and something i can refer back to if i get stuck in the future. 
+This website has been created using Gatsby to document my development in web development and something i can refer back to if i get stuck in the future. </p>
 
-		</p>
+		
 
 		<img src="/images/photoofme.jpg" width="100" height="100" alt="Me" />
 	</>

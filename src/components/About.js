36022df7c1cb186828eceaed7bd54831513471b8
Edita Stralkute <editastralkute@editas-air.home>
2020-04-01 02:46:14 +0100

    jonnydoesdev

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,7 @@ Once i heard about web development in August 2019 i tried out a couple of course
 learning more about it. During that same month i found out that my wife fell pregnant and i knew that i needed to get a job to provide for our growing family. 
 Now that we have had our little baby boy i have managed to put aside all other work again and concentrate on my learning. 
 This website has been created using Gatsby to document my development in web development and something i can refer back to if i get stuck in the future. </p>
+<p> Every bit of work i complete through the course i will be uploading on to Github and will be available from the link below.</p>
 
 		
 
